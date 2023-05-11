75de98900f17a30b993d4a46283ebd95eb53ffac
Chriscrpntr <120032257+Chriscrpntr@users.noreply.github.com>
2023-05-11 14:22:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
 const http = require('http');
 // add the fs library for reading from the file system
 const fs = require('fs');
-// Add Player class
-const Player = require('./player.js');
-const Mage = require('./mage.js');
-const Warrior = require('./warrior.js');
-const Classes = [Mage, Warrior]
-function delay(milliseconds){
-  return new Promise(resolve => {
-      setTimeout(resolve, milliseconds);
-  });
-}
-// New Players
-const Chris = new Classes[0]('Chris', 'Elf', Classes[0]);
-const Kyle = new Classes[1]('Kyle', 'Human', Classes[1]);
-// Create an array of players
-const players = [Chris, Kyle];
-// Add Boss class
-const Boss = require('./boss.js');
-// Create a boss
-const Enemy = new Boss();
-// Combat
-combat = (players, Enemy) => {
-for (
-  let i = 0;
-  i < players.length;
-  i++
-) {
-  Enemy.health = Enemy.health - players[i].attack();
-}
-
-for(let i = 0; i < players.length; i++) {
-  players[i].health - Enemy.attack();
-}
-}
-// Call combat function
-while (Enemy.health > 0){
-  combat(players, Enemy);
-  console.log('Enemy Health: ' + Enemy.health);
-  console.log(players[0].name + ' Health: ' + players[0].health);
-  console.log(players[1].name + ' Health: ' + players[1].health);
-  delay(1000);
-
-}
 // Create a server
 const server = http.createServer(function(request, response) {
   response.statusCode = 200;
@@ -63,3 +21,83 @@ server.listen({ port: 3000, host: 'localhost' }, function() {
   console.log('Server is running!');
 });
 
+
+
+
+// Add Player classes
+const Mage = require('./mage.js');
+const Warrior = require('./warrior.js');
+const Classes = [Mage, Warrior]
+
+// New Players
+const Chris = new Classes[0]('Chris', 'Elf');
+const Kyle = new Classes[1]('Kyle', 'Human');
+// Create an array of players
+const players = [Chris, Kyle];
+
+// Add Boss class
+const Boss = require('./boss.js');
+const Enemy = new Boss();
+
+// Battle Function
+async function Battle (){
+console.log('A wild enemy has appeared!');
+let teamhp = 0;
+for (let i = 0; i < players.length; i++) {
+  teamhp += players[i].health;
+}
+while (Enemy.health >= 0 && teamhp >= 0){
+  // Perform combat round
+  await tick();
+  // Check if enemy is dead
+  if(Enemy.health <= 0){
+  for(i = 0; i < players.length; i++){
+    players[i].experience += 100;
+    console.log(players[i].name + ' has gained 100 experience points!');
+    if(players[i].experience >= 1000){
+      players[i].level += 1;
+      players[i].experience = 0;
+      console.log(players[i].name + ' has leveled up!');
+    }
+  }}
+  // Check if team is dead
+  if(teamhp <= 0){
+    console.log('You have been defeated!');
+    break;
+  }
+
+
+}};
+
+
+// Combat Function
+combat = (players,Enemy) => {
+  for (
+    let i = 0;
+    i < players.length;
+    i++
+  ) {
+    Enemy.health = Enemy.health - players[i].attack();
+  }
+  
+  for(let i = 0; i < players.length; i++) {
+    players[i].health = players[i].health - Enemy.attack();
+  }
+  console.log('Enemy Health: ' + Enemy.health);
+  console.log(players[0].name + ' Health: ' + players[0].health);
+  console.log(players[1].name + ' Health: ' + players[1].health);
+  teamhp = 0;
+  for (let i = 0; i < players.length; i++) {
+     teamhp += players[i].health;
+  }
+};
+async function tick() {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  combat(players,Enemy);
+}
+
+
+
+
+// Start the game
+Battle(players, Enemy);
